3e18e60815de7a561edd329ee8121695fae7ee5a
Cedro23 <33127319+Cedro23@users.noreply.github.com>
2018-01-12 13:45:48 +0100

    Modification du js ajout de la surbrillance pour le joueur 1

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 document.addEventListener("DOMContentLoaded",function(){
     console.log("[DOM] Loaded")
 
-    
+    var player01 = document.querySelectorAll(".Joueur-1")
+    var isPlayer01On = false
 
+
+
+    player01.forEach(function(event){
+      event.addEventListener("click",bgScript)
+    })
+
+    function highlightP01()
+    {
+      if (!isPlayer01On) {
+        for (var i = 0; i < player01.length; i++) {
+          player01[i].style.backgroundColor = "#ff8200"
+        }
+        isPlayer01On = !isPlayer01On
+      } else {
+        for (var i = 0; i < player01.length; i++) {
+          player01[i].style.backgroundColor = "#272A33"
+        }
+        isPlayer01On = !isPlayer01On
+      }
+
+    }
 })
