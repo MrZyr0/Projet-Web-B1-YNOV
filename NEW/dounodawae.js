8de344d7195cc9f0e039445665d8090c13995a1d
Cedro23 <33127319+Cedro23@users.noreply.github.com>
2018-01-16 22:31:43 +0100

    Modif js et css
    +js fait spawn les joueurs
    +css rajout de la classe bracket (à modifier correctement)

--- NEW/dounodawae.js
+++ NEW/dounodawae.js
@@ -22,6 +22,8 @@ document.addEventListener("DOMContentLoaded",function(){
     var pagePlayer = document.querySelector(".Onglet_Joueurs")
     var pageOption = document.querySelector(".Onglet_Parametres")
 
+    //fonctions de changement de page
+
     function switchToBracket()
     {
       if (isTabPlayerActive == true)
@@ -99,4 +101,118 @@ document.addEventListener("DOMContentLoaded",function(){
         isTabOptionActive = true
       }
     }
+
+    //définition de la variable des boutons appliqués
+
+    var btnAppOption = document.querySelector(".Onglet_Parametres_Appliquer")
+    //var btnAppPlayer =
+
+    //event listeners pour les boutons
+
+    btnAppOption.addEventListener("click", updateOption)
+
+
+    //fonctions d'update
+
+    function updateOption()
+    {
+      var numberPlayer = document.querySelector(".Onglet_Parametres_Nb-Joueurs").value
+
+      if (numberPlayer>2)
+      {
+        if (numberPlayer<=4)
+        {
+          var numberBye = 4 - numberPlayer
+          var numberPlayerMax = 4
+          var numberBracketSection = 3
+        }
+        else if (numberPlayer<=8)
+        {
+          var numberBye = 8 - numberPlayer
+          var numberPlayerMax = 8
+          var numberBracketSection = 4
+        }
+        else if (numberPlayer<=16)
+        {
+          var numberBye = 16 - numberPlayer
+          var numberPlayerMax = 16
+          var numberBracketSection = 5
+        }
+        else if (numberPlayer<=32)
+        {
+          var numberBye = 32 - numberPlayer
+          var numberPlayerMax = 32
+          var numberBracketSection = 6
+        }
+        else if (numberPlayer<=64)
+        {
+          var numberBye = 64 - numberPlayer
+          var numberPlayerMax = 64
+          var numberBracketSection = 7
+        }
+        else if (numberPlayer<=128)
+        {
+          var numberBye = 128 - numberPlayer
+          var numberPlayerMax = 128
+          var numberBracketSection = 8
+        }
+        else if (numberPlayer<=256)
+        {
+          var numberBye = 256 - numberPlayer
+          var numberPlayerMax = 256
+          var numberBracketSection = 9
+        }
+      }
+
+      console.log("Players: "+numberPlayer)
+      console.log("Bye: "+numberBye)
+      updateDisplayPlayer(numberPlayerMax, numberPlayer, numberBye, numberBracketSection)
+    }
+
+    function  updateDisplayPlayer(playerMax, player, bye, nbrBrSec)
+    {
+      var zoneInputPlayer = document.querySelector(".Onglet_Joueurs")
+      var templateInputPlayer = document.querySelector("#Template_Joueur").innerHTML
+      var templateBlocPlayer = document.querySelector("#Template_Bloc-Joueurs").innerHTML
+      var templateInZonePlayerInput = document.querySelectorAll(".Onglets_Joueurs_Liste_Pseudo")
+      var templateInZonePlayerBloc = document.querySelectorAll(".Bloc-Joueurs")
+      listPlayer = []
+
+      for (var i = 0; i < templateInZonePlayerInput.length; i++)
+      {
+        templateInZonePlayerInput[i].remove()
+      }
+
+      for (var i = 0; i < templateInZonePlayerBloc.length; i++) {
+        templateInZonePlayerBloc[i].remove()
+      }
+
+      for (var j = 0; j < player; j++)
+      {
+        zoneInputPlayer.insertAdjacentHTML("afterbegin", templateInputPlayer)
+
+        listPlayer[i] = 1
+
+        if (i>player)
+        {
+          listPlayer[i] = 0
+        }
+      }
+
+      for (var i = 0; i < nbrBrSec; i++)
+      {
+        var newSection = document.createElement("div")
+        newSection.className = "Arbre_tour_"+i+" Bracket" 
+
+        document.querySelector(".Onglet_Arbre").appendChild(newSection)
+
+        for (var j = 0; j < playerMax; j++)
+        {
+          var zoneBlocPlayer = document.querySelector(".Arbre_tour_"+i)
+          zoneBlocPlayer.insertAdjacentHTML("afterbegin", templateBlocPlayer)
+        }
+        playerMax = playerMax/2
+      }
+    }
+
 })
