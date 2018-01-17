dd3cd5fa55d98fe5d573401ff773240b3e55953e
Cedro23 <33127319+Cedro23@users.noreply.github.com>
2018-01-18 00:06:00 +0100

    zaesd

--- script.js
+++ script.js
@@ -301,6 +301,8 @@ document.addEventListener("DOMContentLoaded",function(){
 
     function  updateDisplayPlayer(playerMax, player, bye, nbrBrSec)
     {
+      var home = document.querySelector(".Accueil")
+      home.style.display = "none"
       var zoneInputPlayer = document.querySelector(".Onglets_Joueurs_Liste")
       var zoneSectionBracket = document.querySelector(".Onglet_Arbre")
       var templateInputPlayer = document.querySelector("#Template_Joueur").innerHTML
@@ -387,10 +389,8 @@ document.addEventListener("DOMContentLoaded",function(){
     //définition variables Accueil
 
     var btnCreateTournament = document.querySelector(".Fenetre-1_Btn")
-    var btnCreateTrounamentForReal = document.querySelector(".Fenetre-2_Btn")
 
     btnCreateTournament.addEventListener("click", showWindow2)
-    btnCreateTournamentForReal.addEventListener("click", deleteWindow2)
 
     function showWindow2()
     {
@@ -399,12 +399,7 @@ document.addEventListener("DOMContentLoaded",function(){
 
       home1.style.display = "none"
       home2.style.display = "flex"
-    }
 
-    function deleteWindow2()
-    {
-      var home = document.querySelector(".Accueil")
-      home.style.display = "none"
     }
 
 })
